4e53af215f2c0eaf676c1c293e32b08d401cc090
souvikdas <souvik@blend.to>
2025-11-04 15:49:15 +0530

    refactor: Update RootLayout to use dark theme by default and adjust ThemeProvider settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="dark">
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          enableSystem={false}
           disableTransitionOnChange
         >
           {children}
